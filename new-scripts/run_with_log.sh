0ee97709915ff9bc4e7a38fedc1dc60c1d94ac2f
abdur-rafi <abdurrafi403@gmail.com>
2025-09-22 12:24:47 +0600

    script update

--- new-scripts/run_with_log.sh
+++ new-scripts/run_with_log.sh
@@ -24,11 +24,14 @@ vanilla_jar_path="jars/wqfm-tree-vanilla-bug-fixed.jar"
 merged_input_transfer_optimized="jars/merge-input-optimized-transfer.jar"
 reuse_object="jars/wqfm-tree-reuse-objects.jar"
 astral_jar="jars/astral.5.7.8.jar"
+remove_partition_jar="jars/remove-freq-1-partitions.jar"
+
 
 vanilla_output="wqfm-tree-og"
 merged_input_transfer_optimized_output="wqfm-tree-mito"
 reuse_object_output="wqfm-tree-ro"
 astral_output="astral3"
+remove_partition_output="wqfm-tree-rmp"
 
 # for model_cond in "${model_conds[@]}"; do
 #     bash new-scripts/runAstral2DatasetsEst.sh ../astral2Cleaned "$model_cond" $mode 01 02 03 04 05 06 07 08 09 10 > "$log_folder/output.log" 2> "$log_folder/err.err"
@@ -42,10 +45,13 @@ for model_cond in "${model_conds[@]}"; do
     # bash new-scripts/runDataset.sh ../astral2 "$model_cond" "$vanilla_output" \
     # "$vanilla_jar_path" $timestamp 01 02 03 04 05 06 07 08 09 10 > "$log_output" 2> "$log_err"
 
-    bash new-scripts/runDataset_Astral.sh ../astral2 "$model_cond" "$astral_output" \
-    "$astral_jar" "$timestamp" 01 02 03 04 05 06 07 08 09 10 > "$log_output" 2> "$log_err"
+    # bash new-scripts/runDataset_Astral.sh ../astral2 "$model_cond" "$astral_output" \
+    # "$astral_jar" "$timestamp" 01 02 03 04 05 06 07 08 09 10 > "$log_output" 2> "$log_err"
 
 
     # bash new-scripts/runDataset.sh ../astral2 "$model_cond" "$reuse_object_output" \
     # "$reuse_object" $timestamp 01 02 03 04 05 06 07 08 09 10 > "$log_output" 2> "$log_err"
+
+    bash new-scripts/runDataset.sh ../astral2 "$model_cond" "$remove_partition_output" \
+    "$remove_partition_jar" $timestamp 01 02 03 04 05 06 07 08 09 10 > "$log_output" 2> "$log_err"
 done
